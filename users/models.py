4ead754bf789cb31acd0bf9d0eab04f4ebc26b1e
Saloni Kumari <saloni1234.sk@gmail.com>
2025-11-09 14:19:34 +0530

    Implemented real email OTP functionality

--- users/models.py
+++ users/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.utils import timezone
+import datetime
+
 
 def user_directory_path(instance, filename):
-        # file name be named after user 
-        return f'{instance.user.username}/{filename}'
-    
+    # file name be named after user 
+    return f'{instance.user.username}/{filename}'
+
+
 class UserProfile(models.Model):
    
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
@@ -14,14 +18,20 @@ class UserProfile(models.Model):
     education_text = models.TextField(blank=True, null=True)
     
     languages = models.TextField(blank=True, null=True)
-    cv = CloudinaryField( 'file', blank=True, null=True)
+    cv = CloudinaryField('file', blank=True, null=True)
     skills = models.TextField(blank=True, null=True)
     role = models.CharField(max_length=50, blank=True, null=True)
     profile_image = CloudinaryField('image', blank=True, null=True)
     education_image = CloudinaryField('image', blank=True, null=True)
 
-    
-   
+    # OTP fields for password reset
+    password_reset_otp = models.CharField(max_length=6, blank=True, null=True)
+    otp_created_at = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.user.username}'s profile"
+
+    def otp_expired(self):
+        if not self.otp_created_at:
+            return True
+        return timezone.now() > self.otp_created_at + datetime.timedelta(minutes=2)
--- users/urls.py
+++ users/urls.py
@@ -1,15 +1,20 @@
-from django.urls import path , include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView, EmailLoginView, UserProfileView, LogoutView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import (
+    RegisterView,
+    EmailLoginView,
+    UserProfileView,
+    LogoutView,
+    RequestPasswordResetOTPView,
+    PasswordResetView
 )
+
 urlpatterns = [
     path('signup/', RegisterView.as_view(), name='signup'),
     path('login/', EmailLoginView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
     path('profile/', UserProfileView.as_view(), name='user-profile'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('password-reset/request-otp/', RequestPasswordResetOTPView.as_view(), name='request-password-otp'),
+    path('password-reset/confirm/', PasswordResetView.as_view(), name='password-reset'),
 ]
