65158e5467ad8534b7730c9dc525c7980511f019
Mayank Gupta <mayankgupta270606@gmail.com>
2025-11-11 19:25:59 +0530

    Disable welcome email sending on user creation
    Comment out the send_welcome_email function to disable email notifications for new users.

--- users/signals.py
+++ users/signals.py
@@ -5,16 +5,16 @@ from django.core.mail import send_mail
 from django.conf import settings
 
 
-@receiver(post_save, sender=User)
-def send_welcome_email(sender, instance, created, **kwargs):
-    if created:  # Sirf jab new user create ho
-        subject = "Welcome to JobSeeker!"
-        message = f"Hi {instance.first_name},\n\nThank you for registering on JobSeeker portal. We are glad to have you on board!"
-        from_email = settings.EMAIL_HOST_USER
-        recipient_list = [instance.email]
+# @receiver(post_save, sender=User)
+# def send_welcome_email(sender, instance, created, **kwargs):
+#     if created:  # Sirf jab new user create ho
+#         subject = "Welcome to JobSeeker!"
+#         message = f"Hi {instance.first_name},\n\nThank you for registering on JobSeeker portal. We are glad to have you on board!"
+#         from_email = settings.EMAIL_HOST_USER
+#         recipient_list = [instance.email]
 
-        try:
-            send_mail(subject, message, from_email, recipient_list)
-        except Exception as e:
-            print(f"Error sending welcome email to {instance.email}: {e}")
+#         try:
+#             send_mail(subject, message, from_email, recipient_list)
+#         except Exception as e:
+#             print(f"Error sending welcome email to {instance.email}: {e}")
             
